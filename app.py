bd5d5f2a64bd36bc64b05c1e2bec8a2aae8908c6
Sunil Sonar <sunil.r.sonar@gmail.com>
2025-03-31 13:13:02 +0000

    somple k8s app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+PORT = os.environ.get('PORT', 8000)
+
+@app.route('/')
+def index():
+
+    env = dict(os.environ)
+
+    return render_template('index.html', env = env)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=PORT, host='0.0.0.0')
